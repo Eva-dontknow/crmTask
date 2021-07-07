14498860e9df69a4e25da164ff802e071a774e6e
win10 <2356443494@qq.com>
2021-07-07 09:24:24 +0800

    添加了其他基本控制

--- src/main/java/com/nast/crm/controller/BaseController.java
+++ src/main/java/com/nast/crm/controller/BaseController.java
@@ -1,6 +1,7 @@
 package com.nast.crm.controller;
 
 
+import com.nast.base.ResultInfo;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,21 +17,21 @@ public class BaseController {
     }
 
 
-//    public ResultInfo success(){
-//        return new ResultInfo();
-//    }
-//
-//    public ResultInfo success(String msg){
-//        ResultInfo resultInfo= new ResultInfo();
-//        resultInfo.setMsg(msg);
-//        return resultInfo;
-//    }
-//
-//    public ResultInfo success(String msg,Object result){
-//        ResultInfo resultInfo= new ResultInfo();
-//        resultInfo.setMsg(msg);
-//        resultInfo.setResult(result);
-//        return resultInfo;
-//    }
+    public ResultInfo success(){
+        return new ResultInfo();
+    }
+
+    public ResultInfo success(String msg){
+        ResultInfo resultInfo= new ResultInfo();
+        resultInfo.setMsg(msg);
+        return resultInfo;
+    }
+
+    public ResultInfo success(String msg,Object result){
+        ResultInfo resultInfo= new ResultInfo();
+        resultInfo.setMsg(msg);
+        resultInfo.setResult(result);
+        return resultInfo;
+    }
 
 }
