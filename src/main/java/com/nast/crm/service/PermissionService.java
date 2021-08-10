64410c8f2ae59ab7667dca4a7d7ba68f5e8904ab
loujing <2356443494@qq.com>
2021-08-10 11:38:40 +0800

    update .gitignore

---
+++ src/main/java/com/nast/crm/service/PermissionService.java
@@ -0,0 +1,24 @@
+package com.nast.crm.service;
+
+import com.nast.base.BaseService;
+import com.nast.crm.dao.PermissionMapper;
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import java.security.acl.Permission;
+import java.util.List;
+
+@Service
+public class PermissionService extends BaseService<Permission,Integer> {
+
+    @Resource
+    private PermissionMapper permissionMapper;
+
+    /***
+     * 通过查询用户拥有的角色，角色拥有的资源，得到用户拥有的资源列表 （资源权限码）
+     * @param userId
+     * @return java.util.List<java.lang.String>
+     */
+    public List<String> queryUserHasRoleHasPermissionByUserId(Integer userId) {
+        return permissionMapper.queryUserHasRoleHasPermissionByUserId(userId);
+    }
+}
